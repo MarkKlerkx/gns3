d35f0c9180b1260dcd2cf510e8b46a31e6820620
MarkKlerkx <mark@ascent-it.nl>
2026-02-18 13:23:48 +0100

    Refactor GNS3 server update script for clarity

--- gns3_server_updates.sh
+++ gns3_server_updates.sh
@@ -11,29 +11,26 @@ GITHUB_URL="https://raw.githubusercontent.com/MarkKlerkx/gns3/refs/heads/main/gn
 
 echo "--- GNS3 Master Sync: $SOURCE_DIR naar netwerk $NETWORK.x ---"
 
-# 1. Lijst van images (alleen .qcow2)
-IMAGES=$(ls "$SOURCE_DIR" | grep ".qcow2")
+# 1. Haal de lijst van images op
+IMAGES=$(ls "$SOURCE_DIR" | grep -E ".qcow2|.md5sum")
 
 for i in {1..254}; do
     IP="$NETWORK.$i"
     
-    # Check of de host leeft
     if ping -c 1 -W 1 "$IP" > /dev/null 2>&1; then
         echo "----------------------------------------------------"
-        echo "[$IP] Poging tot inloggen..."
+        echo -n "[$IP] Inlogpoging... "
 
-        # --- DEEL A: TEST VERBINDING & SCRIPT UPDATE ---
-        # We voeren een simpele check uit en vangen de exit code op ($?)
-        sshpass -p "$PASS" ssh -o StrictHostKeyChecking=no -o ConnectTimeout=5 "$USER@$IP" "exit" 2>/dev/null
-        
-        if [ $? -ne 0 ]; then
-            echo "[$IP] FOUT: Inloggen mislukt (Verkeerd wachtwoord of SSH-key issue). Overslaan..."
-            continue  # Spring direct naar de volgende 'i' in de loop
+        # CRUCIALE STAP: Test de verbinding. 
+        # We proberen een simpel commando ('true'). Als dit faalt, skippen we ALLES voor deze IP.
+        if ! sshpass -p "$PASS" ssh -o StrictHostKeyChecking=no -o ConnectTimeout=2 -o BatchMode=no "$USER@$IP" "true" 2>/dev/null; then
+            echo "GEWEIGERD (Permission Denied). Host wordt overgeslagen."
+            continue
         fi
 
-        echo "[$IP] Ingelogd. Bezig met controles..."
+        echo "GEACCEPTEERD. Start verwerking..."
 
-        # Update het script
+        # --- DEEL A: SCRIPT UPDATE ---
         sshpass -p "$PASS" ssh -o StrictHostKeyChecking=no "$USER@$IP" << EOF
             if [ -f /usr/local/bin/gns3_monitor.sh ]; then
                 CURRENT_V=\$(grep 'VERSION=' /usr/local/bin/gns3_monitor.sh | cut -d'"' -f2)
@@ -46,15 +43,13 @@ EOF
 
         # --- DEEL B: IMAGE SYNC ---
         for IMG in $IMAGES; do
-            # Check of file bestaat op remote host
-            sshpass -p "$PASS" ssh -o StrictHostKeyChecking=no "$USER@$IP" "[ -f $DEST_DIR/$IMG ]" 2>/dev/null
-            
-            if [ $? -ne 0 ]; then
-                echo "[$IP] ! Kopieer ontbrekende image: $IMG"
+            # Check of de image al bestaat op de remote host
+            if ! sshpass -p "$PASS" ssh -o StrictHostKeyChecking=no "$USER@$IP" "[ -f $DEST_DIR/$IMG ]" 2>/dev/null; then
+                echo "  -> Kopieer: $IMG"
                 sshpass -p "$PASS" scp -o StrictHostKeyChecking=no "$SOURCE_DIR/$IMG" "$USER@$IP:$DEST_DIR/"
                 sshpass -p "$PASS" ssh -o StrictHostKeyChecking=no "$USER@$IP" "sudo chown gns3:gns3 $DEST_DIR/$IMG"
             else
-                echo "[$IP] OK: $IMG reeds aanwezig."
+                echo "  -> OK: $IMG aanwezig."
             fi
         done
     fi
